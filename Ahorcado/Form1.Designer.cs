6f787dbf919c271ca4686786f33461390c89cbed
Joel García <joel.garcia.wk@gmail.com>
2022-02-14 19:31:27 +0100

    Agregar archivos de proyecto.

---
+++ Ahorcado/Form1.Designer.cs
@@ -0,0 +1,428 @@
+﻿namespace Ahorcado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.letraA = new System.Windows.Forms.Button();
+            this.letraB = new System.Windows.Forms.Button();
+            this.letraC = new System.Windows.Forms.Button();
+            this.letraD = new System.Windows.Forms.Button();
+            this.letraE = new System.Windows.Forms.Button();
+            this.letraF = new System.Windows.Forms.Button();
+            this.letraG = new System.Windows.Forms.Button();
+            this.letraN = new System.Windows.Forms.Button();
+            this.letraM = new System.Windows.Forms.Button();
+            this.letraL = new System.Windows.Forms.Button();
+            this.letraK = new System.Windows.Forms.Button();
+            this.letraJ = new System.Windows.Forms.Button();
+            this.letraI = new System.Windows.Forms.Button();
+            this.letraH = new System.Windows.Forms.Button();
+            this.letraT = new System.Windows.Forms.Button();
+            this.letraS = new System.Windows.Forms.Button();
+            this.letraR = new System.Windows.Forms.Button();
+            this.letraQ = new System.Windows.Forms.Button();
+            this.letraP = new System.Windows.Forms.Button();
+            this.letraO = new System.Windows.Forms.Button();
+            this.letraÑ = new System.Windows.Forms.Button();
+            this.letraZ = new System.Windows.Forms.Button();
+            this.letraY = new System.Windows.Forms.Button();
+            this.letraX = new System.Windows.Forms.Button();
+            this.letraW = new System.Windows.Forms.Button();
+            this.letraV = new System.Windows.Forms.Button();
+            this.letraU = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "_ _ _ _ _";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseMnemonic = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Ahorcado.Properties.Resources.ahorcado_0;
+            this.pictureBox1.Location = new System.Drawing.Point(109, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(177, 202);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // letraA
+            // 
+            this.letraA.Location = new System.Drawing.Point(27, 316);
+            this.letraA.Name = "letraA";
+            this.letraA.Size = new System.Drawing.Size(44, 42);
+            this.letraA.TabIndex = 3;
+            this.letraA.Text = "a";
+            this.letraA.UseVisualStyleBackColor = true;
+            this.letraA.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraB
+            // 
+            this.letraB.Location = new System.Drawing.Point(77, 316);
+            this.letraB.Name = "letraB";
+            this.letraB.Size = new System.Drawing.Size(44, 42);
+            this.letraB.TabIndex = 4;
+            this.letraB.Text = "b";
+            this.letraB.UseVisualStyleBackColor = true;
+            this.letraB.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraC
+            // 
+            this.letraC.Location = new System.Drawing.Point(127, 316);
+            this.letraC.Name = "letraC";
+            this.letraC.Size = new System.Drawing.Size(44, 42);
+            this.letraC.TabIndex = 5;
+            this.letraC.Text = "c";
+            this.letraC.UseVisualStyleBackColor = true;
+            this.letraC.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraD
+            // 
+            this.letraD.Location = new System.Drawing.Point(177, 316);
+            this.letraD.Name = "letraD";
+            this.letraD.Size = new System.Drawing.Size(44, 42);
+            this.letraD.TabIndex = 6;
+            this.letraD.Text = "d";
+            this.letraD.UseVisualStyleBackColor = true;
+            this.letraD.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraE
+            // 
+            this.letraE.Location = new System.Drawing.Point(227, 316);
+            this.letraE.Name = "letraE";
+            this.letraE.Size = new System.Drawing.Size(44, 42);
+            this.letraE.TabIndex = 7;
+            this.letraE.Text = "e";
+            this.letraE.UseVisualStyleBackColor = true;
+            this.letraE.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraF
+            // 
+            this.letraF.Location = new System.Drawing.Point(277, 316);
+            this.letraF.Name = "letraF";
+            this.letraF.Size = new System.Drawing.Size(44, 42);
+            this.letraF.TabIndex = 8;
+            this.letraF.Text = "f";
+            this.letraF.UseVisualStyleBackColor = true;
+            this.letraF.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraG
+            // 
+            this.letraG.Location = new System.Drawing.Point(327, 316);
+            this.letraG.Name = "letraG";
+            this.letraG.Size = new System.Drawing.Size(44, 42);
+            this.letraG.TabIndex = 9;
+            this.letraG.Text = "g";
+            this.letraG.UseVisualStyleBackColor = true;
+            this.letraG.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraN
+            // 
+            this.letraN.Location = new System.Drawing.Point(327, 374);
+            this.letraN.Name = "letraN";
+            this.letraN.Size = new System.Drawing.Size(44, 42);
+            this.letraN.TabIndex = 16;
+            this.letraN.Text = "n";
+            this.letraN.UseVisualStyleBackColor = true;
+            this.letraN.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraM
+            // 
+            this.letraM.Location = new System.Drawing.Point(277, 374);
+            this.letraM.Name = "letraM";
+            this.letraM.Size = new System.Drawing.Size(44, 42);
+            this.letraM.TabIndex = 15;
+            this.letraM.Text = "m";
+            this.letraM.UseVisualStyleBackColor = true;
+            this.letraM.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraL
+            // 
+            this.letraL.Location = new System.Drawing.Point(227, 374);
+            this.letraL.Name = "letraL";
+            this.letraL.Size = new System.Drawing.Size(44, 42);
+            this.letraL.TabIndex = 14;
+            this.letraL.Text = "l";
+            this.letraL.UseVisualStyleBackColor = true;
+            this.letraL.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraK
+            // 
+            this.letraK.Location = new System.Drawing.Point(177, 374);
+            this.letraK.Name = "letraK";
+            this.letraK.Size = new System.Drawing.Size(44, 42);
+            this.letraK.TabIndex = 13;
+            this.letraK.Text = "k";
+            this.letraK.UseVisualStyleBackColor = true;
+            this.letraK.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraJ
+            // 
+            this.letraJ.Location = new System.Drawing.Point(127, 374);
+            this.letraJ.Name = "letraJ";
+            this.letraJ.Size = new System.Drawing.Size(44, 42);
+            this.letraJ.TabIndex = 12;
+            this.letraJ.Text = "j";
+            this.letraJ.UseVisualStyleBackColor = true;
+            this.letraJ.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraI
+            // 
+            this.letraI.Location = new System.Drawing.Point(77, 374);
+            this.letraI.Name = "letraI";
+            this.letraI.Size = new System.Drawing.Size(44, 42);
+            this.letraI.TabIndex = 11;
+            this.letraI.Text = "i";
+            this.letraI.UseVisualStyleBackColor = true;
+            this.letraI.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraH
+            // 
+            this.letraH.Location = new System.Drawing.Point(27, 374);
+            this.letraH.Name = "letraH";
+            this.letraH.Size = new System.Drawing.Size(44, 42);
+            this.letraH.TabIndex = 10;
+            this.letraH.Text = "h";
+            this.letraH.UseVisualStyleBackColor = true;
+            this.letraH.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraT
+            // 
+            this.letraT.Location = new System.Drawing.Point(327, 431);
+            this.letraT.Name = "letraT";
+            this.letraT.Size = new System.Drawing.Size(44, 42);
+            this.letraT.TabIndex = 23;
+            this.letraT.Text = "t";
+            this.letraT.UseVisualStyleBackColor = true;
+            this.letraT.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraS
+            // 
+            this.letraS.Location = new System.Drawing.Point(277, 431);
+            this.letraS.Name = "letraS";
+            this.letraS.Size = new System.Drawing.Size(44, 42);
+            this.letraS.TabIndex = 22;
+            this.letraS.Text = "s";
+            this.letraS.UseVisualStyleBackColor = true;
+            this.letraS.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraR
+            // 
+            this.letraR.Location = new System.Drawing.Point(227, 431);
+            this.letraR.Name = "letraR";
+            this.letraR.Size = new System.Drawing.Size(44, 42);
+            this.letraR.TabIndex = 21;
+            this.letraR.Text = "r";
+            this.letraR.UseVisualStyleBackColor = true;
+            this.letraR.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraQ
+            // 
+            this.letraQ.Location = new System.Drawing.Point(177, 431);
+            this.letraQ.Name = "letraQ";
+            this.letraQ.Size = new System.Drawing.Size(44, 42);
+            this.letraQ.TabIndex = 20;
+            this.letraQ.Text = "q";
+            this.letraQ.UseVisualStyleBackColor = true;
+            this.letraQ.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraP
+            // 
+            this.letraP.Location = new System.Drawing.Point(127, 431);
+            this.letraP.Name = "letraP";
+            this.letraP.Size = new System.Drawing.Size(44, 42);
+            this.letraP.TabIndex = 19;
+            this.letraP.Text = "p";
+            this.letraP.UseVisualStyleBackColor = true;
+            this.letraP.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraO
+            // 
+            this.letraO.Location = new System.Drawing.Point(77, 431);
+            this.letraO.Name = "letraO";
+            this.letraO.Size = new System.Drawing.Size(44, 42);
+            this.letraO.TabIndex = 18;
+            this.letraO.Text = "o";
+            this.letraO.UseVisualStyleBackColor = true;
+            this.letraO.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraÑ
+            // 
+            this.letraÑ.Location = new System.Drawing.Point(27, 431);
+            this.letraÑ.Name = "letraÑ";
+            this.letraÑ.Size = new System.Drawing.Size(44, 42);
+            this.letraÑ.TabIndex = 17;
+            this.letraÑ.Text = "ñ";
+            this.letraÑ.UseVisualStyleBackColor = true;
+            this.letraÑ.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraZ
+            // 
+            this.letraZ.Location = new System.Drawing.Point(303, 488);
+            this.letraZ.Name = "letraZ";
+            this.letraZ.Size = new System.Drawing.Size(44, 42);
+            this.letraZ.TabIndex = 29;
+            this.letraZ.Text = "z";
+            this.letraZ.UseVisualStyleBackColor = true;
+            this.letraZ.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraY
+            // 
+            this.letraY.Location = new System.Drawing.Point(253, 488);
+            this.letraY.Name = "letraY";
+            this.letraY.Size = new System.Drawing.Size(44, 42);
+            this.letraY.TabIndex = 28;
+            this.letraY.Text = "y";
+            this.letraY.UseVisualStyleBackColor = true;
+            this.letraY.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraX
+            // 
+            this.letraX.Location = new System.Drawing.Point(203, 488);
+            this.letraX.Name = "letraX";
+            this.letraX.Size = new System.Drawing.Size(44, 42);
+            this.letraX.TabIndex = 27;
+            this.letraX.Text = "x";
+            this.letraX.UseVisualStyleBackColor = true;
+            this.letraX.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraW
+            // 
+            this.letraW.Location = new System.Drawing.Point(153, 488);
+            this.letraW.Name = "letraW";
+            this.letraW.Size = new System.Drawing.Size(44, 42);
+            this.letraW.TabIndex = 26;
+            this.letraW.Text = "w";
+            this.letraW.UseVisualStyleBackColor = true;
+            this.letraW.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraV
+            // 
+            this.letraV.Location = new System.Drawing.Point(103, 488);
+            this.letraV.Name = "letraV";
+            this.letraV.Size = new System.Drawing.Size(44, 42);
+            this.letraV.TabIndex = 25;
+            this.letraV.Text = "v";
+            this.letraV.UseVisualStyleBackColor = true;
+            this.letraV.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // letraU
+            // 
+            this.letraU.Location = new System.Drawing.Point(53, 488);
+            this.letraU.Name = "letraU";
+            this.letraU.Size = new System.Drawing.Size(44, 42);
+            this.letraU.TabIndex = 24;
+            this.letraU.Text = "u";
+            this.letraU.UseVisualStyleBackColor = true;
+            this.letraU.Click += new System.EventHandler(this.LetraPulsada);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 555);
+            this.Controls.Add(this.letraZ);
+            this.Controls.Add(this.letraY);
+            this.Controls.Add(this.letraX);
+            this.Controls.Add(this.letraW);
+            this.Controls.Add(this.letraV);
+            this.Controls.Add(this.letraU);
+            this.Controls.Add(this.letraT);
+            this.Controls.Add(this.letraS);
+            this.Controls.Add(this.letraR);
+            this.Controls.Add(this.letraQ);
+            this.Controls.Add(this.letraP);
+            this.Controls.Add(this.letraO);
+            this.Controls.Add(this.letraÑ);
+            this.Controls.Add(this.letraN);
+            this.Controls.Add(this.letraM);
+            this.Controls.Add(this.letraL);
+            this.Controls.Add(this.letraK);
+            this.Controls.Add(this.letraJ);
+            this.Controls.Add(this.letraI);
+            this.Controls.Add(this.letraH);
+            this.Controls.Add(this.letraG);
+            this.Controls.Add(this.letraF);
+            this.Controls.Add(this.letraE);
+            this.Controls.Add(this.letraD);
+            this.Controls.Add(this.letraC);
+            this.Controls.Add(this.letraB);
+            this.Controls.Add(this.letraA);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button letraA;
+        private System.Windows.Forms.Button letraB;
+        private System.Windows.Forms.Button letraC;
+        private System.Windows.Forms.Button letraD;
+        private System.Windows.Forms.Button letraE;
+        private System.Windows.Forms.Button letraF;
+        private System.Windows.Forms.Button letraG;
+        private System.Windows.Forms.Button letraN;
+        private System.Windows.Forms.Button letraM;
+        private System.Windows.Forms.Button letraL;
+        private System.Windows.Forms.Button letraK;
+        private System.Windows.Forms.Button letraJ;
+        private System.Windows.Forms.Button letraI;
+        private System.Windows.Forms.Button letraH;
+        private System.Windows.Forms.Button letraT;
+        private System.Windows.Forms.Button letraS;
+        private System.Windows.Forms.Button letraR;
+        private System.Windows.Forms.Button letraQ;
+        private System.Windows.Forms.Button letraP;
+        private System.Windows.Forms.Button letraO;
+        private System.Windows.Forms.Button letraÑ;
+        private System.Windows.Forms.Button letraZ;
+        private System.Windows.Forms.Button letraY;
+        private System.Windows.Forms.Button letraX;
+        private System.Windows.Forms.Button letraW;
+        private System.Windows.Forms.Button letraV;
+        private System.Windows.Forms.Button letraU;
+    }
+}
+
